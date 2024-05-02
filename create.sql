90c21247ecffb741c0d9c4c2bfd284c5bfc312ea
Jack <133031155+JackJack7890@users.noreply.github.com>
2024-05-02 18:11:58 -0400

    Update create.sql

--- create.sql
+++ create.sql
@@ -1,1 +1,27 @@
+DROP DATABASE IF EXISTS video_games;
+CREATE DATABASE video_games;
+USE video_games;
 
+CREATE TABLE IF NOT EXISTS games (
+    Name VARCHAR(255) NOT NULL,
+    Release_Year INT(5),
+    Publisher VARCHAR(255),
+    Developer VARCHAR(255),
+    Amount_Of_Players VARCHAR(255),
+    Genre VARCHAR(255),
+    PRIMARY KEY (Name)
+);
+
+CREATE TABLE IF NOT EXISTS publishers (
+    Name VARCHAR(255) NOT NULL,
+    Founded DATE,
+    Headquarters VARCHAR(255),
+    PRIMARY KEY (Name)
+);
+
+CREATE TABLE IF NOT EXISTS developers (
+    Name VARCHAR(255) NOT NULL,
+    Founded DATE,
+    Headquarters VARCHAR(255),
+    PRIMARY KEY (Name)
+);
